bf044d63a885f528ee7c6c8a42b9287cffa48723
Alexander Ponomarenko <aponomarenko@mtt.ru>
2020-06-27 01:09:16 +0300

    start adding async action

--- src/components/App/reducer.ts
+++ src/components/App/reducer.ts
@@ -11,7 +11,9 @@ const COLOR_BLUE = 'blue';
 const COLOR_RED = 'red';
 
 type ChangeColorAction = { type: string; payload: { color: string } };
-type ActionCreator = (param?: string) => ChangeColorAction;
+type IncAction = { type: string; payload: {} };
+type ActionCreator = (param?: string) => ChangeColorAction | IncAction;
+
 export const setBlue: ActionCreator = () => ({
     type: SET_COLOR,
     payload: { color: COLOR_BLUE }
@@ -20,17 +22,35 @@ export const setRed: ActionCreator = () => ({
     type: SET_COLOR,
     payload: { color: COLOR_RED }
 });
+export const incAsync: ActionCreator = () => ({
+    type: 'INCREMENT_ASYNC',
+    payload: {}
+});
+export const inc: ActionCreator = () => ({
+    type: 'INCREMENT',
+    payload: {}
+});
 
-export function reducer(state: AppState = defaultState, action: ChangeColorAction): AppState {
+export function reducer(
+    state: AppState = defaultState,
+    action: ChangeColorAction | IncAction
+): AppState {
     switch (action.type) {
         case SET_COLOR: {
+            const a = action as ChangeColorAction;
             return {
                 ...state,
-                color: action.payload.color
+                color: a.payload.color
+            };
+        }
+        case 'INCREMENT': {
+            return {
+                ...state
+                // color: action.payload.color
             };
         }
     }
     return state;
 }
 
-export const actions = { setBlue, setRed };
+export const actions = { setBlue, setRed, incAsync, inc };
